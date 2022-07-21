dbe592f3b54313949a7fce5c982b42f4f9045af6
Ecem Ozturk <ecemozturk704@gmail.com>
2022-07-21 17:10:04 +0300

    Add files via upload

---
+++ ptk_3_1_varAndLet.playground/Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+var greeting = "Hello, playground" //değişken tanımlama
+let pi = 3.14 // sabit tanımlama
+
+//pi'yi float olarak tanımlayalım
+let pi2 : Float = 3.14
+
+//boş değişken ve sabit tanımlama
+var bos : Int
+let bosluk : String
+
+//Ekrana yazdırma
+var isim = "Ecem"
+print (greeting + isim)
+
+//Ters slash + parantez = swift değişken olarak algılar
+print("Hoşgeldin, \(isim)")
+
+//array tanımlama
+var yaslar = [12,13,14]
+var isimler = ["Ali","Ahmet","Mehmet"]
+//Ahmeti yazdır
+print("\(isimler[1])")
+//Ali, 14 yaşındadır.
+print("\(isimler[0]), \(yaslar[2]) yaşındadır.")
+
+
+
+
