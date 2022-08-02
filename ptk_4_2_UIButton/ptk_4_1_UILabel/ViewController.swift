3a5e2a2971f196193e3b7617a0f7b3d20a93f0e6
Ecem Ozturk <ecemozturk704@gmail.com>
2022-08-02 15:22:59 +0300

    Add files via upload

---
+++ ptk_4_2_UIButton/ptk_4_1_UILabel/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  ptk_4_1_UILabel
+//
+//  Created by Ecem Öztürk on 25.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var label2: UILabel!
+
+    @IBOutlet weak var label: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        label.text = "Merhaba IOS"
+        label.font = UIFont.systemFont(ofSize: 17.0)
+        label.textColor = UIColor.red
+    }
+
+    @IBAction func button(_ sender: Any) {
+        //print("tapped")
+        label2.text="Hoş Geldin!"
+    }
+    
+}
+
